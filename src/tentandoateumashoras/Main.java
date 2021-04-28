11c4c0d1c0bd8a2e619d04ddc312a7362f29c5fe
ProjetoJapa <64323974+ProjetoJapa@users.noreply.github.com>
2021-04-28 19:56:15 -0300

    Add files via upload

---
+++ src/tentandoateumashoras/Main.java
@@ -0,0 +1,22 @@
+package tentandoateumashoras;
+
+/* ArvoreAvl.java
+ * Arvore AVL 
+*@ALUNO: KLEYSON DINIZ MOUREIRA
+ * INSTITUICAO: Faculdade Internacional da Paraiba (FPB)
+ * CURSO: CCO - 4 Período
+ * OBJETIVO: Realização de AVL - JAVA (Atividade N1)
+ * DESENVOLVEDOR : Kleyson Diniz Moureira
+ * LOCAL: Centro/FPB - João Pessoa/PB - Brasil
+ * DATA: 25/04/2021 (Criação)
+ * ULTIMA MODIFICACAO: 27/04/2021 às 01h24min AM (24h)
+@CONTATO:	Email: kleysondiniz@gmail.com */
+
+public class Main {
+	public static void main(String args[]) {
+
+		AvlTree Arvore = new AvlTree();
+		Arvore.IniciarMenu();
+
+	}
+}
